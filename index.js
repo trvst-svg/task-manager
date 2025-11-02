5237a33be0d837ed258c99663d403ac516a8985c
Suprasiddha Maharjan <suppu@Suprasiddhas-MacBook-Air.local>
2025-11-02 15:29:24 +0545

    secret key add

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express';
 const app = express();
 
-const port = 5000
+const port = 5000;
 
 app.use(express.json);
-app.use()
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
